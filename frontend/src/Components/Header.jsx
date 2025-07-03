3929aa13279cd0ff668ca69f087548c932b8c041
LanceMontierro <lancemontierro21@gmail.com>
2025-07-03 12:04:56 +0800

    fix styles

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,8 +1,31 @@
 import { useAppContext } from "../../context/appContext";
 import { Logo } from "../assets";
 import { CiMenuFries } from "react-icons/ci";
+import { useState, useEffect } from "react";
+import { IoMdClose } from "react-icons/io";
+import { FaGithub, FaInstagram } from "react-icons/fa";
+import { navs } from "../../const";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
-  const { user } = useAppContext();
+  const { user, active, setActive } = useAppContext();
+  const [openNavbar, setOpenNavbar] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleNavbar = () => {
+    setOpenNavbar((prev) => !prev);
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = openNavbar ? "hidden" : "";
+  }, [openNavbar]);
+
+  const handleActive = (name) => {
+    setActive(name);
+    setOpenNavbar(false);
+    document.body.style.overflow = "";
+    navigate(`/${name.toLowerCase()}`);
+  };
+
   return (
     <header>
       <div className="px-4 py-2 mx-auto flexBetween max-w-7xl ">
@@ -13,9 +36,72 @@ const Header = () => {
           </span>
         </div>
 
-        <div className="block p-2 border rounded-lg cursor-pointer md:hidden border-cardDark">
+        <div
+          className="block p-2 border rounded-lg cursor-pointer md:hidden border-cardDark"
+          onClick={toggleNavbar}
+        >
           <CiMenuFries size={18} className="text-light" />
         </div>
+
+        <div
+          className={`absolute top-0 h-full z-10 flex flex-col items-center gap-3 px-4 py-2 bg-white md:hidden w-full transition-all ease-in-out duration-500 
+               ${openNavbar ? "right-0" : "right-[-100%]"} `}
+        >
+          <div className="w-full flex justify-end">
+            <div
+              className="block p-2 border rounded-lg cursor-pointer md:hidden border-cardDark"
+              onClick={() => setOpenNavbar(false)}
+            >
+              <IoMdClose size={20} />
+            </div>
+          </div>
+          <div className="flex flex-col items-center w-full mt-2 gap-3">
+            {navs.map((nav, i) => {
+              const Icon = nav.icon;
+              return (
+                <button
+                  key={i}
+                  onClick={() => handleActive(nav.name)}
+                  className={`flex items-center py-3 px-2 gap-2 rounded-2xl text-light w-full justify-center  ${
+                    active === nav.name ? "bg-cardDark border-[#fff] " : ""
+                  }`}
+                >
+                  <Icon className="text-[22px]" />
+                  <span className=" text-textSm font-medium font-AudioWide">
+                    {nav.name}
+                  </span>
+                </button>
+              );
+            })}
+
+            <a
+              className={
+                "flex items-center py-3 px-2 gap-2 rounded-2xl text-light w-full justify-center  border-[#fff] "
+              }
+              href="https://github.com/LanceMontierro"
+              target="_blank"
+            >
+              <FaGithub className="text-[22px]" />
+              <span className=" text-textSm font-medium font-AudioWide">
+                Github
+              </span>
+            </a>
+
+            <a
+              className={
+                "flex items-center py-3 px-2 gap-2 rounded-2xl text-light w-full justify-center  border-[#fff] "
+              }
+              href="https://www.instagram.com/lncemntierro/"
+              target="_blank"
+            >
+              <FaInstagram className="text-[22px]" />
+              <span className=" text-textSm font-medium font-AudioWide">
+                Instagram
+              </span>
+            </a>
+          </div>
+        </div>
+
         <div className="items-center hidden gap-3 px-4 py-2 rounded-2xl bg-cardDark hover:bg-primary md:flex">
           {user && user.imageUrl ? (
             <img src={user.imageUrl} alt="" className="w-8 h-8 rounded-full" />
--- frontend/src/Pages/Archived.jsx
+++ frontend/src/Pages/Archived.jsx
@@ -25,68 +25,72 @@ const Archived = () => {
           <h2 className="text-textMd font-bold mt-4 max-[821px]:text-center max-[500px]:text-[22px]">
             Archived Notes
           </h2>
-          <div className="relative flex flex-col flex-wrap gap-3 mt-2"></div>
-          {archivedNotes && archivedNotes.length > 0 ? (
-            archivedNotes.map((note) => (
-              <div key={note._id && note._id} className="relative">
-                <div className="w-full py-4 pl-6 pr-1 rounded-lg bg-cardDark flexBetween">
-                  <div className="flex items-center w-full gap-6">
-                    <button>
-                      <RiInboxArchiveLine size={20} />
+          <div className="relative flex flex-col flex-wrap gap-3 mt-2">
+            {archivedNotes && archivedNotes.length > 0 ? (
+              archivedNotes.map((note) => (
+                <div key={note._id && note._id} className="relative">
+                  <div className="w-full py-4 pl-6 pr-1 rounded-lg bg-cardDark flexBetween">
+                    <div className="flex items-center w-full gap-6">
+                      <button>
+                        <RiInboxArchiveLine size={20} />
+                      </button>
+
+                      <div className="flex flex-col justify-center w-full gap-1">
+                        <p>{note.title}</p>
+                        <p className="text-gray-500 text-[12px]">
+                          {note.createdAt && note.createdAt.split("T")[0]}
+                        </p>
+                      </div>
+                    </div>
+
+                    <button
+                      className="mr-5 cursor-pointer"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        handleShopwOptions(note._id);
+                      }}
+                    >
+                      <BsThreeDotsVertical
+                        size={20}
+                        className="text-gray-500"
+                      />
                     </button>
 
-                    <div className="flex flex-col justify-center w-full gap-1">
-                      <p>{note.title}</p>
-                      <p className="text-gray-500 text-[12px]">
-                        {note.createdAt && note.createdAt.split("T")[0]}
-                      </p>
-                    </div>
+                    {showOptions[note._id] && (
+                      <div className="absolute right-0 bottom-[-70px] bg-cardDark rounded-lg shadow-lg z-200">
+                        <button
+                          className="flex items-center w-full gap-2 px-4 py-4 text-left rounded-lg hover:bg-gray-200"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            archivedNote(note);
+                          }}
+                        >
+                          <RiInboxArchiveLine size={20} />
+                          <p>Unarchive Note</p>
+                        </button>
+
+                        <button
+                          className="flex items-center w-full gap-2 px-4 py-4 text-left rounded-lg hover:bg-gray-200"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            deleteNote(note._id);
+                          }}
+                        >
+                          <FaTrashCan size={20} />
+                          <p>Delete Note</p>
+                        </button>
+                      </div>
+                    )}
                   </div>
-
-                  <button
-                    className="mr-5 cursor-pointer"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      e.preventDefault();
-                      handleShopwOptions(note._id);
-                    }}
-                  >
-                    <BsThreeDotsVertical size={20} className="text-gray-500" />
-                  </button>
-
-                  {showOptions[note._id] && (
-                    <div className="absolute right-0 bottom-[-70px] bg-cardDark rounded-lg shadow-lg z-200">
-                      <button
-                        className="flex items-center w-full gap-2 px-4 py-4 text-left rounded-lg hover:bg-gray-200"
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          e.preventDefault();
-                          archivedNote(note);
-                        }}
-                      >
-                        <RiInboxArchiveLine size={20} />
-                        <p>Unarchive Note</p>
-                      </button>
-
-                      <button
-                        className="flex items-center w-full gap-2 px-4 py-4 text-left rounded-lg hover:bg-gray-200"
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          e.preventDefault();
-                          deleteNote(note._id);
-                        }}
-                      >
-                        <FaTrashCan size={20} />
-                        <p>Delete Note</p>
-                      </button>
-                    </div>
-                  )}
                 </div>
-              </div>
-            ))
-          ) : (
-            <p className=" text-gray-500 mt-4">No archived notes found.</p>
-          )}
+              ))
+            ) : (
+              <p className=" text-gray-500 mt-4">No archived notes found.</p>
+            )}
+          </div>
         </div>
       </div>
     </>
